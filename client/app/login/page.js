20d4feffffd47f9348e6c709defff416e1e0fa8c
Aasim <syedaasimali334@gmail.com>
2026-02-19 12:54:42 +0530

    update

--- client/app/login/page.js
+++ client/app/login/page.js
@@ -170,7 +170,7 @@ function LoginForm() {
                             variant="outline"
                             className="w-full btn-outline h-14 font-black"
                         >
-                            <a href={`${process.env.NEXT_PUBLIC_API_URL}/api/oauth/google${isExtension ? '?extension=true' : ''}`}>
+                            <a href={`${process.env.NEXT_PUBLIC_API_URL}/oauth/google${isExtension ? '?extension=true' : ''}`}>
                                 <svg className="w-5 h-5 mr-3" viewBox="0 0 24 24">
                                     <path
                                         d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
